47e3fb91f2b8bb78f8dca71edb58302c0423e110
romain menu <rmenu@spiria.com>
2023-07-12 17:14:26 -0400

    IG-21: adding MSSQL container IG-48: adding redis commander container

--- golibro/settings.py
+++ golibro/settings.py
@@ -24,7 +24,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mailhog' # Mailhog Container
 EMAIL_PORT = '1025'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
