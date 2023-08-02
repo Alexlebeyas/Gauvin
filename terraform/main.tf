db36bcd81c5a078f9430996d38685d923e4e5a4c
Joshua Majonis <jmajonis@spiria.com>
2023-08-02 14:38:24 -0400

    Update for Gauvin Azure account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
+  skip_provider_registration = true
 }
 
 terraform {
@@ -11,8 +12,8 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "terraform-state-testing"
-    storage_account_name = "terraformojayxkjtim"
+    resource_group_name  = "terraform"
+    storage_account_name = "terraformstategauvin"
     container_name       = "envs"
     key                  = "terraform.tfstate"
   }
@@ -20,24 +21,24 @@ terraform {
 
 
 resource "azurerm_resource_group" "resource-group" {
-  name     = "gauvin-${terraform.workspace}"
+  name     = "gauvin-rg-${terraform.workspace}"
   location = var.location
 }
 
-resource "azurerm_log_analytics_workspace" "log-analytics-workspace" {
-  name                = "log-analytics-workspace-${terraform.workspace}"
-  location            = azurerm_resource_group.resource-group.location
-  resource_group_name = azurerm_resource_group.resource-group.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
+# resource "azurerm_log_analytics_workspace" "log-analytics-workspace" {
+#   name                = "log-analytics-workspace-${terraform.workspace}"
+#   location            = azurerm_resource_group.resource-group.location
+#   resource_group_name = azurerm_resource_group.resource-group.name
+#   sku                 = "PerGB2018"
+#   retention_in_days   = 30
+# }
 
-resource "azurerm_container_app_environment" "container-app-environment" {
-  name                       = "container-app-environment-${terraform.workspace}"
-  location                   = azurerm_resource_group.resource-group.location
-  resource_group_name        = azurerm_resource_group.resource-group.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.log-analytics-workspace.id
-}
+# resource "azurerm_container_app_environment" "container-app-environment" {
+#   name                       = "container-app-environment-${terraform.workspace}"
+#   location                   = azurerm_resource_group.resource-group.location
+#   resource_group_name        = azurerm_resource_group.resource-group.name
+#   log_analytics_workspace_id = azurerm_log_analytics_workspace.log-analytics-workspace.id
+# }
 
 data "azurerm_client_config" "current" {}
 
@@ -62,33 +63,33 @@ data "azurerm_client_config" "current" {}
 #   ]
 # }
 
-resource "azurerm_container_app" "qa" {
-  count                        = terraform.workspace == "non-prod" ? 1 : 0
-  name                         = "container-app-qa"
-  container_app_environment_id = azurerm_container_app_environment.container-app-environment.id
-  resource_group_name          = azurerm_resource_group.resource-group.name
-  revision_mode                = "Single"
+# resource "azurerm_container_app" "qa" {
+#   count                        = terraform.workspace == "non-prod" ? 1 : 0
+#   name                         = "container-app-qa"
+#   container_app_environment_id = azurerm_container_app_environment.container-app-environment.id
+#   resource_group_name          = azurerm_resource_group.resource-group.name
+#   revision_mode                = "Single"
 
-  template {
-    min_replicas = 1
-    max_replicas = 1
+#   template {
+#     min_replicas = 1
+#     max_replicas = 1
 
-    container {
-      name   = "container-app-qa"
-      image  = "nginxdemos/hello:latest"
-      cpu    = 0.25
-      memory = "0.5Gi"
-    }
-  }
+#     container {
+#       name   = "container-app-qa"
+#       image  = "nginxdemos/hello:latest"
+#       cpu    = 0.25
+#       memory = "0.5Gi"
+#     }
+#   }
 
-  ingress {
-    allow_insecure_connections = false
-    external_enabled           = true
-    target_port                = 80
-    traffic_weight {
-      percentage = 100
-    }
-  }
-}
+#   ingress {
+#     allow_insecure_connections = false
+#     external_enabled           = true
+#     target_port                = 80
+#     traffic_weight {
+#       percentage = 100
+#     }
+#   }
+# }
 
 
